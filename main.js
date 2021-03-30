d0f3f9aea5ef1566389e6ed98b99841e10553120
Natasha194 <77156649+Natasha194@users.noreply.github.com>
2021-03-30 18:13:46 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -37,7 +37,70 @@ function modelLoaded() {
 
 
 console.log(ml5.version);
-classifier = ml5.imgClassifier("https://teachablemachine.withgoogle.com/models/aPGAY20bP/model.json", modelLoaded);
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/9RGJqC7ct/model.json", modelLoaded);
 
 
 
+
+
+
+
+function check() {
+    var img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+    console.error(error);
+    }
+    else {
+        console.log(results);
+        document.getElementById("result_gesture_name").innerHTML = results[0].label;
+        document.getElementById("result_gesture_name2").innerHTML = results[1].label;
+        prediction1 = results[0].label;
+        prediction2 = results[1].label;
+        speak();
+
+        if (results[0].label == "Alien") {
+            document.getElementById("update_gesture").innerHTML = "&#128406";
+        }
+
+        if (results[0].label == "Fist Bump") {
+            document.getElementById("update_gesture").innerHTML = "&#9994";
+        }
+
+        if (results[0].label == "Thumbs Up") {
+            document.getElementById("update_gesture").innerHTML = "&#128077";
+        }
+
+        if (results[0].label == "Peace") {
+            document.getElementById("update_gesture").innerHTML = "&#9996";
+        }
+
+        if (results[0].label == "OK/Yes") {
+            document.getElementById("update_gesture").innerHTML = "&#128076";
+        }
+
+
+        if (results[1].label == "Fist Bump") {
+            document.getElementById("update_gesture2").innerHTML = "&#9994";
+        }
+
+        if (results[1].label == "Alien") {
+            document.getElementById("update_gesture2").innerHTML = "&#128406";
+        }
+
+        if (results[1].label == "Thumbs Up") {
+            document.getElementById("update_gesture2").innerHTML = "&#128077";
+        }
+
+        if (results[1].label == "Peace") {
+            document.getElementById("update_gesture2").innerHTML = "&#9996";
+        }
+
+        if (results[1].label == "OK/Yes") {
+            document.getElementById("update_gesture").innerHTML = "&#128076";
+        }
+    }
+}
